51d72edef3005c0792952a0c7a7873690d81f522
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to customize a panel's context menu via actions
+
+
+<p>This example shows how to customize a panel's context menu via customization actions.<br />
+Menu customization actions are added via the DockLayoutManager.ContextMenuCustomizations collection.</p><p></p>
+
+<br/>
+
+
---
+++ VB/DXDocking_PanelContextMenu_Actions/MainPage.xaml.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Namespace DXDocking_PanelContextMenu_Actions
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub bAbout_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+			MessageBox.Show("About Window")
+		End Sub
+
+	End Class
+End Namespace
